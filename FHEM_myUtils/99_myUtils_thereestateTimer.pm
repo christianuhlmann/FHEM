030a1c5ab57ced3ab6178c38b7491deb913a2655
christian <christian@T500>
2016-08-07 14:12:12 +0200

    zwischenstand

--- FHEM_myUtils/99_myUtils_thereestateTimer.pm
+++ FHEM_myUtils/99_myUtils_thereestateTimer.pm
@@ -12,17 +12,23 @@ use POSIX;
 use HTTP::Date;
 use DateTime;
 
+####################################################
 sub
 myUtils_thereestateTimer_Initialize($$)
 {
   my ($hash) = @_;
 }
+# end sub myUtils_thereestateTimer_Initialize($$)
+####################################################
 
-# Enter you functions below _this_ line.
-sub mapthereestatetimer($$) {
+
+####################################################
+sub
+mapthereestatetimer($$) {
 	my ($dev, $state) = @_;
+	my $hash = {};
 	my $statevirt = $state;
-	
+
     if ($defs{$dev}) {
 		my $devtype=AttrVal($dev,'subType','');
 
@@ -38,13 +44,47 @@ sub mapthereestatetimer($$) {
 				my $virt_dev_state = ReadingsVal("$devvirt","virt_state","unknown");
 
 				if($virt_dev_state ne $state) {
-					fhem("set $devvirt postEvent $statevirt");
-					fhem("setreading  $devvirt virt_state $statevirt");
-					Log 1, ("mapthereestatetimer ($devvirt): $statevirt");
+   	InternalTimer(gettimeofday()+3, "mapthereestatewithtime", $hash, 0);
 				}
 			}
     	}
+   elsif ($devtype eq "threeStateSensor") {
+   	InternalTimer(gettimeofday()+30, "mapthereestatewithtime", $hash, 0);
+   }
 	}
 }
+
+sub mapthereestatewithtime($$$) {
+	my ($hash,$dev, $state) = @_;
+	my $statevirt = $state;
+
+    if ($defs{$dev}) {
+		my $devtype=AttrVal($dev,'subType','');
+
+	  	if ($devtype eq "threeStateSensor") {
+    		Log 1, ("mapthereestatetimer ($dev): $state");
+			my $devvirt=$dev.".virt_WindowRec";
+
+			if ($defs{$devvirt}) {
+				if ($state eq "opened") 	{ $statevirt = "open"; }
+			 	elsif ($state eq "tilted")	{ $statevirt = "open"; }
+			 	elsif ($state eq "close")	{ $statevirt = "closed"; } 
+			 	else 						{ $statevirt = $state; }
+				my $virt_dev_state = ReadingsVal("$devvirt","virt_state","unknown");
+
+				if($virt_dev_state ne $state) {
+#					fhem("set $devvirt postEvent $statevirt");
+#					fhem("setreading  $devvirt virt_state $statevirt");
+					Log 1, ("mapthereestatewithtime");
+				}
+			}
+    	}
+   elsif ($devtype eq "threeStateSensor2") {
+   	InternalTimer(gettimeofday()+30, "Log 1, ("HAHA")", $hash, 0);
+   }
+	}
+}
+
+
 
 1;
